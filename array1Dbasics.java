894f597630d7b08ff6a6b73c3db235953a40a628
mukhtar4532 <mukhtarhussain4532@gmail.com>
2024-12-06 22:17:06 +0530

    commit

--- array1Dbasics.java
+++ array1Dbasics.java
@@ -15,12 +15,13 @@ class Array1Dbasics {
 
   // Binary Search
   public static int binarySearch(int arr[], int key) {
-    int start = 0, end = arr.length;
+    int start = 0, end = arr.length - 1;
     while (start <= end) {
-      int mid = (start + end) / 3;
+      int mid = start + (end - start) / 2;
       if (arr[mid] == key) {
         return mid;
-      } else if (arr[mid] > key) {
+      }
+      if (arr[mid] > key) {
         end = mid - 1;
       } else {
         start = mid + 1;
@@ -29,20 +30,96 @@ class Array1Dbasics {
     return -1;
   }
 
+  // Reverse of an Array
+  public static void reverseArray(int arr[]) {
+    int start = 0, end = arr.length - 1;
+    while (start < end) {
+      int temp = arr[end];
+      arr[end] = arr[start];
+      arr[start] = temp;
+      start++;
+      end--;
+    }
+  }
+
+  // Pairs in an Array
+  public static void printPairs(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        System.out.print("(" + arr[i] + ", " + arr[j] + ")");
+      }
+      System.out.println();
+    }
+  }
+
+  // Print Subarrays
+  public static void printSubarrays(int arr[]) {
+    int total = 0;
+    for (int i = 0; i < arr.length; i++) {
+      int start = i;
+      for (int j = i; j < arr.length; j++) {
+        int end = j;
+        for (int k = start; k < end; k++) {
+          System.out.print(arr[k] + ", ");
+          total++;
+        }
+        System.out.println();
+      }
+    }
+    System.out.println("Total subarrays " + total);
+  }
+
+  // Print max Subarrays sum
+  public static void subArraySum(int arr[]) {
+    int total = 0;
+    int maxSum = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      int start = i;
+      for (int j = i; j < arr.length; j++) {
+        int end = j;
+        int currSum = 0;
+        for (int k = start; k < end; k++) {
+          System.out.print(arr[k] + ",  ");
+          currSum += arr[k];
+          total++;
+        }
+        System.out.print("CurrSum " + currSum);
+        if (currSum > maxSum) {
+          maxSum = currSum;
+        }
+        System.out.println();
+      }
+    }
+    System.out.println("Maximum sum is " + maxSum);
+    System.out.println("Total subarrays " + total);
+  }
+
   public static void main(String[] args) {
     // System.out.println("Hello");
-    Scanner sc = new Scanner(System.in);
+    // Scanner sc = new Scanner(System.in);
 
     // int arr[] = { 3, 4, 15, 6, 7 };
     // largestNumber(arr);
 
-    int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
-    int key = 11;
-    int index = binarySearch(arr, key);
-    if (index == -1) {
-      System.out.println("NOT FOUND");
-    } else {
-      System.out.println("Key at index " + index);
-    }
+    // int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+    // int key = 11;
+    // int index = binarySearch(arr, key);
+    // if (index == -1) {
+    // System.out.println("NOT FOUND");
+    // } else {
+    // System.out.println("Key at index " + index);
+    // }
+
+    // int arr[] = { 1, 2, 3, 4 };
+    // reverseArray(arr);
+    // for (int i = 0; i < arr.length; i++) {
+    // System.out.print(arr[i] + " ");
+    // }
+
+    int arr[] = { 2, 4, 6, 8, 10 };
+    // printPairs(arr);
+    // printSubarrays(arr);
+    subArraySum(arr);
+
   }
 }
