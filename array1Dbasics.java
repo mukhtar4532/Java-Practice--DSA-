a571ff1d95cd71206e80d279ddb7409e35292ba1
mukhtar4532 <mukhtarhussain4532@gmail.com>
2024-12-08 23:56:04 +0530

    commit

--- array1Dbasics.java
+++ array1Dbasics.java
@@ -69,7 +69,7 @@ class Array1Dbasics {
     System.out.println("Total subarrays " + total);
   }
 
-  // Print max Subarrays sum
+  // Print max Subarrays sum ----->>>>>> O(n^3)
   public static void subArraySum(int arr[]) {
     int total = 0;
     int maxSum = Integer.MIN_VALUE;
@@ -94,6 +94,35 @@ class Array1Dbasics {
     System.out.println("Total subarrays " + total);
   }
 
+  // Print max Subarrays sum ----->>>>>> O(n^2)
+  public static void subArraySum2(int arr[]) {
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    int prefix[] = new int[arr.length];
+
+    prefix[0] = arr[0];
+
+    for (int i = 1; i < prefix.length; i++) {
+      // calculate prefix array
+      prefix[i] = prefix[i - 1] + arr[i];
+
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      int start = i;
+      for (int j = i; j < arr.length; j++) {
+        int end = j;
+        currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+        if (currSum > maxSum) {
+          maxSum = currSum;
+        }
+      }
+    }
+
+    System.out.println("maxSum = " + maxSum);
+
+  }
+
   public static void main(String[] args) {
     // System.out.println("Hello");
     // Scanner sc = new Scanner(System.in);
@@ -116,10 +145,11 @@ class Array1Dbasics {
     // System.out.print(arr[i] + " ");
     // }
 
-    int arr[] = { 2, 4, 6, 8, 10 };
+    int arr[] = { 1, -2, 6, -1, 3 };
     // printPairs(arr);
     // printSubarrays(arr);
-    subArraySum(arr);
+    // subArraySum(arr);
+    subArraySum2(arr);
 
   }
 }
